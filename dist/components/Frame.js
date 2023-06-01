58ebdb9e494ee12bb0ac79466fe40b2189d884a5
siddharth <siddharth@apples-MacBook-Pro.local>
2023-06-01 11:53:34 +0530

    v2

--- dist/components/Frame.js
+++ dist/components/Frame.js
@@ -1,5 +1,7 @@
 "use strict";
 
+require("core-js/modules/web.dom-collections.iterator.js");
+
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
@@ -7,11 +9,15 @@ exports.default = void 0;
 
 require("core-js/modules/es.promise.js");
 
-var _react = require("react");
+var _react = _interopRequireWildcard(require("react"));
 
 var _hashconnect = require("hashconnect");
 
 var _sdk = require("@hashgraph/sdk");
+
+function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function _getRequireWildcardCache() { return cache; }; return cache; }
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
 const Frame = props => {
   let hashConnect = new _hashconnect.HashConnect(); //variable to hold an instance of Hashconnect
@@ -88,7 +94,7 @@ const Frame = props => {
       } catch (e) {}
     };
   }, []);
-  return /*#__PURE__*/React.createElement("iframe", {
+  return /*#__PURE__*/_react.default.createElement("iframe", {
     id: "hbar-shop-hedera-iframe",
     src: "http://localhost:3001/embed?shop=jithendra-test-store.myshopify.com",
     height: props.height || "672px",
